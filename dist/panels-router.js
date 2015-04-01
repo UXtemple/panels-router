a095ed194afce1a73a6669bd8fb6855e4e463f23
Darío Javier Cravero <dario@uxtemple.com>
2015-04-01 18:33:00 +0100

    Updated dependencies. Bumped version to v0.1.1

--- dist/panels-router.js
+++ dist/panels-router.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.PanelsRouter = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 "use strict";
 
 var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["default"] : obj; };
@@ -44,7 +44,7 @@ var _interopRequire = function (obj) { return obj && obj.__esModule ? obj["defau
 
 var Marty = _interopRequire(require("marty"));
 
-module.exports = Marty.createConstants(["ROUTER_GO_BACK", "ROUTER_NAVIGATE"]);
+module.exports = Marty.createConstants(["ROUTER_NAVIGATE"]);
 
 },{"marty":undefined}],3:[function(require,module,exports){
 "use strict";
@@ -254,4 +254,5 @@ var RouterStore = (function (_Marty$Store) {
 
 module.exports = Marty.register(RouterStore);
 
-},{"./constants":2,"./parser":6,"immutable":undefined,"marty":undefined}]},{},[4]);
+},{"./constants":2,"./parser":6,"immutable":undefined,"marty":undefined}]},{},[4])(4)
+});
